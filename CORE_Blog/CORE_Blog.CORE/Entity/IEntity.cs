7778b17b90c96bc1b481a0cb1cda4d43f13059ec
Dilara Özkaynak <dilaraaozk@hotmail.com>
2021-01-11 14:17:51 +0300

    Proje yapısı oluşturuldu Core katmanı kodlaması tamamlandı

---
+++ CORE_Blog/CORE_Blog.CORE/Entity/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CORE_Blog.CORE.Entity
+{
+    public interface IEntity<T>
+    {
+         T ID { get; set; }
+    }
+}
